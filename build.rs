22ad53ded3d39f41901b2b3107f716931199f3f8
realzvqle <eyefort@gmail.com>
2024-05-02 13:07:46 -0500

    added resources

---
+++ build.rs
@@ -0,0 +1,16 @@
+use {
+    std::{
+        env,
+        io,
+    },
+    winres::WindowsResource,
+};
+
+fn main() -> io::Result<()> {
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            .set_resource_file("resources/resource.rc")
+            .compile()?;
+    }
+    Ok(())
+}
